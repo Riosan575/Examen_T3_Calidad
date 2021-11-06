d224429f38f2e6c7ac91801a4573c143d9923164
Riosan575 <an_99_ben@hotmail.com>
2021-11-05 19:27:00 -0500

    first commit

---
+++ Examen_T3_Calidad/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Examen_T3_Calidad.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
